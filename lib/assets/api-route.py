2e5f9dd505cc0c96fa1a44021381b9e84107f1de
ATAKONE\atevilson.araujo <atevilson.araujo@atak.com.br>
2024-05-10 17:26:49 -0300

    ajuste do host para emular app externamente

--- lib/assets/api-route.py
+++ lib/assets/api-route.py
@@ -4,15 +4,18 @@ import requests
 
 app = Flask(__name__)
 
+
 class Pesquisa:
     def __init__(self):
         self.titulo = ""
         self.link = ""
 
+
 @app.route('/api/pesquisa/<string:dado_de_pesquisa>', methods=['GET'])
 def api_pesquisa(dado_de_pesquisa):
     pesquisa_google = get_pesquisa(dado_de_pesquisa)
     return jsonify({"titulo": pesquisa_google.titulo, "link": pesquisa_google.link})
+
 
 def get_pesquisa(dado_de_pesquisa):
     link_pesquisa = f"https://www.google.com/search?q={dado_de_pesquisa}"
@@ -31,5 +34,6 @@ def get_pesquisa(dado_de_pesquisa):
 
     return pesquisar
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False)
