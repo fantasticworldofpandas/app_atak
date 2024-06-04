407046094d144aadf547c5e6db46126ed0ec08ef
atevilson <freitas.atevilson@gmail.com>
2024-06-04 11:47:40 -0300

    Modularização - search_provider

---
+++ commons_dependencies/android/app/src/main/kotlin/com/example/commons_dependencies/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.commons_dependencies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ commons_dependencies/lib/commons_dependencies.dart
@@ -0,0 +1,4 @@
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:dio/dio.dart';
+export 'package:provider/provider.dart';
+export 'package:commons_dependencies/search_provider.dart';
