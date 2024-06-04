8ef772c95619a92cdf4123fa4ad636d826f85332
atevilson <freitas.atevilson@gmail.com>
2024-06-04 11:52:41 -0300

    Modularização - app_widget, app_module

---
+++ core/lib/app/app_module.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:module_home/home_screen.dart';
+
+class AppModule extends Module {
+  @override
+  void routes(RouteManager r) {
+    r.child('/', child: (args) => HomeScreen());
+  }
+}
