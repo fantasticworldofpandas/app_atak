29896fdba7a06454c191891e7e576b40c3e56fe6
atevilson <freitas.atevilson@gmail.com>
2024-06-04 11:39:59 -0300

    Modularização - result_container

---
+++ commons/android/app/src/main/kotlin/com/example/commons/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.commons
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ commons/lib/commons.dart
@@ -0,0 +1,3 @@
+export 'package:dio/dio.dart';
+export 'package:provider/provider.dart';
+export 'package:url_launcher/url_launcher.dart';
