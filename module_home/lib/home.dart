c659df41ee9017dd9701e076a8e55b0f9af09595
atevilson <freitas.atevilson@gmail.com>
2024-06-11 14:34:18 -0300

    atualizando para rota modular

--- module_home/lib/home.dart
+++
@@ -1,6 +0,0 @@
-export 'package:commons_dependencies/search_provider.dart';
-export 'package:provider/provider.dart';
-export 'package:commons/result_container.dart';
-export 'package:commons/commons.dart';
-export 'package:flutter/material.dart';
-export 'package:module_home/home_screen.dart';
